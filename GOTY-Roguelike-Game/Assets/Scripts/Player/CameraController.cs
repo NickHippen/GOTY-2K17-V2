a4394b4e2f71f6c0812b53217fdd2442b36accd2
Nick Hippen <nhippen973@gmail.com>
2017-09-23 17:42:12 -0500

    Added camera controller & mock player

---
+++ GOTY-Roguelike-Game/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private const float Y_ANGLE_MIN = 0f;
+	private const float Y_ANGLE_MAX = 30f;
+	private const float ZOOM_MIN = 5f;
+	private const float ZOOM_MAX = 25f;
+
+	public Transform lookAt;
+
+	private Camera cam;
+	private Transform camTransform;
+
+	private float currentX = 0f;
+	private float currentY = 0f;
+	private float currentZoom = 200f;
+	private float sensitivityX = 4f;
+	private float sensitivityY = 1f;
+	private float sensitivityZoom = 50f;
+
+	void Start () {
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+
+		camTransform = transform;
+		cam = Camera.main;
+	}
+
+	private void Update() {
+		currentX += Input.GetAxis("Mouse X");
+		currentY -= Input.GetAxis("Mouse Y");
+		currentZoom -= Input.GetAxis("Mouse ScrollWheel") * sensitivityZoom;
+
+		currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+		currentZoom = Mathf.Clamp(currentZoom, ZOOM_MIN, ZOOM_MAX);
+	}
+
+	void LateUpdate () {
+		Vector3 dir = new Vector3(0, 1, -currentZoom);
+		Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+		camTransform.position = lookAt.position + rotation * dir;
+
+		// Face player
+		camTransform.LookAt(lookAt.position);
+	}
+}
