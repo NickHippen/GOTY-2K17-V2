42166bc24dd501aa917ec34cc7ed82aac624e02c
Nick Hippen <Nick Hippen>
2017-10-23 16:38:41 -0500

    Made health bar rotate towards camera at all times

---
+++ GOTY-Roguelike-Game/Assets/Scripts/Utility/PointTowardsCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTowardsCamera : MonoBehaviour {
+
+	public Camera camera;
+
+	private void Start() {
+		if (camera == null) {
+			camera = Camera.main;
+		}
+	}
+
+	void LateUpdate () {
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, camera.transform.rotation, float.MaxValue);
+	}
+}
