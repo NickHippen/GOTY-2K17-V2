8676e899060c448634081913d8eb8c684bf85241
Kevin Chen <chenk.dev@gmail.com>
2017-11-20 16:36:39 -0600

    Mark for Death and Turret abilities implemented

--- GOTY-Roguelike-Game/Assets/Scripts/Player/Abilities/Gunslinger/Trap.cs
+++ GOTY-Roguelike-Game/Assets/Scripts/Player/Abilities/Gunslinger/Trap.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grenade : Ability
+public class Trap : Ability
 {
     public float damage;
     public GrenadeObject grenadeObject;
