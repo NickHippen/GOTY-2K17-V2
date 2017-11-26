282336c0401aead158ea008aaea7662aa47aff23
TokensPony <cmtherealmccoy@gmail.com>
2017-11-26 17:40:16 -0600

    Added particle effects to all Berserker Abilities

--- GOTY-Roguelike-Game/Assets/Scripts/Player/Abilities/Berserker/Adrenaline.cs
+++ GOTY-Roguelike-Game/Assets/Scripts/Player/Abilities/Berserker/Adrenaline.cs
@@ -6,6 +6,8 @@ public class Adrenaline : Ability {
     
     public float damageMultiplier = 2f;
     public float duration = 5f;
+	public ParticleSystem effect;
+	public float effectDistance;
 
 	void Start(){
 		//ApplyOnFrame = true;
@@ -13,9 +15,13 @@ public class Adrenaline : Ability {
 
     public override void applyEffect(GameObject player)
     {
+		this.transform.position = player.transform.position + player.transform.forward * effectDistance;
+
         foreach(GameObject weapon in player.GetComponent<PlayerInventory>().weapons)
         {
             weapon.GetComponent<WeaponData>().ApplyDamageMultiplier(duration, damageMultiplier);
         }
+
+		effect.Emit (50);
     }
 }
