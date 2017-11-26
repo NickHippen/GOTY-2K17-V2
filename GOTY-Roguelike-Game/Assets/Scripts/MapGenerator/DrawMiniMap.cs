d08f3dee1483254d192a164ccaf42e559fe7f6fe
NICK <nrichman962@gmail.com>
2017-11-26 01:25:55 -0600

    New minimap generation algorithm

--- GOTY-Roguelike-Game/Assets/Scripts/MapGenerator/DrawMiniMap.cs
+++ GOTY-Roguelike-Game/Assets/Scripts/MapGenerator/DrawMiniMap.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.IO;
 public class DrawMiniMap : MonoBehaviour {
 
 	public GameObject wallIcon;
-	public GameObject doorIcon;
-	public GameObject hallIcon;
-	public GameObject roomIcon;
+
 	public GameObject playerIcon;
 
 	Vector3 v3;
@@ -16,35 +14,71 @@ public class DrawMiniMap : MonoBehaviour {
 		GameObject map = GameObject.Find ("MiniMap");
 		GameObject toInstantiate = null;
 		GameObject instance = null;
+	
 		Vector3 v3;
+		float offset = 10f;
+		float offset2 = 5f;
 		for (int i = 0; i < maparr.GetLength(0); i++) {
-			for (int j = 0; j < maparr.GetLength(1); j++) {
-				switch (maparr [i, j]) {
-				case "wall":
-					toInstantiate = wallIcon;
-					break;
-				case "hall":
-					toInstantiate = hallIcon;
-					break;
-				case "room":
-					toInstantiate = roomIcon;
-					break;
-				case "door":
-					toInstantiate = doorIcon;
-					break;
-				default:
-					toInstantiate = wallIcon;
-					break;
+			for (int j = 0; j < maparr.GetLength (1); j++) {
+				toInstantiate = wallIcon;
+
+				if (maparr [i, j] == "hall") {
+					//Left wall
+					if (i - 1 >= 0 && maparr [i - 1, j] == "wall") {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.identity) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset, j * offset, 0f);
+					}
+					//Right wall
+					if ((i + 1 < maparr.GetLength (0) && maparr [i + 1, j] == "wall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.identity) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset, j * offset, 0f);
+					}
+					//Above wall
+					if ((j - 1 >= 0 && maparr [i, j - 1] == "wall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.Euler (0, 0, 90f)) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset2, j * offset - offset2, 0f);
+					}
+					//Below wall
+					if ((j + 1 < maparr.GetLength (1) && maparr [i, j + 1] == "wall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.Euler (0, 0, 90f)) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset2, j * offset + offset2, 0f);
+					}
 				}
-				instance = Instantiate (toInstantiate, new Vector3 (i * 5f, j * 5f, 0f), Quaternion.identity) as GameObject;
-				instance.transform.SetParent (map.transform);
-				instance.transform.localPosition = new Vector3 (i * 5f, j * 5f, 0f);
-				v3 = instance.transform.position;
-				v3.y -= 200;
-				instance.transform.position = v3;
+
+				if (maparr [i, j] == "room") {
+					//Left wall
+					if (i - 1 >= 0 && maparr [i - 1, j] == "wall" || maparr [i - 1, j] == "hall") {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.identity) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset, j * offset, 0f);
+					}
+					//Right wall
+					if ((i + 1 < maparr.GetLength (0) && maparr [i + 1, j] == "wall" || maparr [i + 1, j] == "hall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.identity) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset, j * offset, 0f);
+					}
+					//Above wall
+					if ((j - 1 >= 0 && maparr [i, j - 1] == "wall" || maparr [i, j - 1] == "hall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.Euler (0, 0, 90f)) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset2, j * offset - offset2, 0f);
+					}
+					//Below wall
+					if ((j + 1 < maparr.GetLength (1) && maparr [i, j + 1] == "wall" || maparr [i, j + 1] == "hall")) {
+						instance = Instantiate (toInstantiate, new Vector3 (0f, 0f, 0f), Quaternion.Euler (0, 0, 90f)) as GameObject;
+						instance.transform.SetParent (map.transform);
+						instance.transform.localPosition = new Vector3 (i * offset + offset2, j * offset + offset2, 0f);
+					}
+				}
 			}
+
 		}
-		instance = Instantiate (playerIcon, new Vector3 (0 * 5f, 0 * 5f, 0f), Quaternion.identity) as GameObject;
+		instance = Instantiate (playerIcon, new Vector3 (0 * offset, 0 * offset, 0f), Quaternion.identity) as GameObject;
 		instance.transform.SetParent (map.transform);
 
 	}
@@ -52,14 +86,7 @@ public class DrawMiniMap : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//Draws exit portal
-		Vector3 exitPos = GameObject.Find ("Exit Portal").transform.GetChild (0).transform.localPosition;
-		GameObject instance = Instantiate (hallIcon, new Vector3 (exitPos.x* 5 / 8f, exitPos.z * 5 / 8f, 0f), Quaternion.identity);
-		instance.transform.SetParent (GameObject.Find("MiniMap").transform);
-		instance.transform.localPosition = new Vector3 (exitPos.x* 5 / 8f, exitPos.z * 5 / 8f, 0f);
-		v3 = instance.transform.position;
-		v3.y -= 200;
-		instance.transform.position = v3;
+
 	}
 	
 	// Update is called once per frame
