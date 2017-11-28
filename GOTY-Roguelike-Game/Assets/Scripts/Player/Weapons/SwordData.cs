b103536017b7d805cb48cfbd34319c129170b018
Kevin Chen <chenk.dev@gmail.com>
2017-11-28 01:37:08 -0600

    comment out sword changes due to not working properly

--- GOTY-Roguelike-Game/Assets/Scripts/Player/Weapons/SwordData.cs
+++ GOTY-Roguelike-Game/Assets/Scripts/Player/Weapons/SwordData.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class SwordData : WeaponData
 {
-    public float hitDuration = 0.1f;
-    private bool hit;
+    // commented code was not working properly
+    //public float hitDuration = 0.1f;
+    //private bool hit;
 
-    public override void Attack()
-    {
-        hit = true;
-        StartCoroutine(HitDuration());
-    }
+    //public override void Attack()
+    //{
+    //    hit = true;
+    //    StartCoroutine(HitDuration());
+    //}
 
     void OnTriggerEnter(Collider collision) {
-        if (hit) {
+        //if (hit) {
             RigCollider rigCollider = collision.gameObject.GetComponent<RigCollider>();
             if (rigCollider != null && !(rigCollider is AttackCollider) && rigCollider.RootUnit is AggressiveUnit) {
                 AggressiveUnit monster = ((AggressiveUnit)rigCollider.RootUnit);
@@ -24,12 +25,12 @@ public class SwordData : WeaponData
                 damage = WeaponModifierActionHandler.GetOnDamageAction(modifier)(this, monster, damage);
                 monster.Damage(damage);
             }
-        }
+        //}
 	}
 
-    IEnumerator HitDuration()
-    {
-        yield return new WaitForSeconds(hitDuration);
-        hit = false;
-    }
+    //IEnumerator HitDuration()
+    //{
+    //    yield return new WaitForSeconds(hitDuration);
+    //    hit = false;
+    //}
 }
