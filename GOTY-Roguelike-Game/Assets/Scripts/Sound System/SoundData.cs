cd381d79486a066414d6c9f5c340453555e2bc05
TokensPony <cmtherealmccoy@gmail.com>
2017-11-30 16:56:27 -0600

    First test with walking sound. It's rough but its there, Will continue to refine.

---
+++ GOTY-Roguelike-Game/Assets/Scripts/Sound System/SoundData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundData : MonoBehaviour {
+
+	public List<AudioClip> soundEffects;
+	AudioSource source;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		
+	}
+
+	public virtual void playSound(int index){
+		source.PlayOneShot (soundEffects [index]);
+	}
+}
