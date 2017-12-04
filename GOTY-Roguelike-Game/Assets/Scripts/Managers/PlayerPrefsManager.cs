359c6111082e706b0be3a037dcc68e2daec9e0fb
NICK <nrichman962@gmail.com>
2017-12-03 21:35:28 -0600

    Sound slider in options

---
+++ GOTY-Roguelike-Game/Assets/Scripts/Managers/PlayerPrefsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+
+	public static void SetMasterVolume(float volume){
+		if (volume >= 0f && volume <= 1f) {
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		} else {
+			Debug.LogError ("Master volume out of range");
+		}
+	}
+
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+}
---
+++ GOTY-Roguelike-Game/Assets/Scripts/Managers/SoundControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundControl: MonoBehaviour {
+
+	public Slider volumeSlider;
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+		volumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
+		SetDefaults ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		musicManager.ChangeVolume (volumeSlider.value);
+	}
+
+	public void SaveAndExit(){
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+	}
+
+	public void SetDefaults(){
+		volumeSlider.value = 0.8f;
+	}
+}
