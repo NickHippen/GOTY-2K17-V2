0ea3b63b9be4fd0e886ff2c47008c0f6b5ef308b
Kevin Chen <chenk.dev@gmail.com>
2017-12-10 16:28:40 -0600

    missed merging some files

---
+++ GOTY-Roguelike-Game/Assets/Scripts/Units/BossUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BossUnit : AggressiveUnit {
+
+	// Bosses must have aggro before they will take damage
+	public override void Damage(float amount) {
+		if (this.target == null) {
+			return;
+		}
+		base.Damage(amount);
+	}
+
+	public override void Damage(float amount, Transform attacker) {
+		if (this.target == null) {
+			return;
+		}
+		base.Damage(amount, attacker);
+	}
+
+}
