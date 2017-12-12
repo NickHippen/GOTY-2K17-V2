1cc3f1a9b284e7303deb80d02326fb6831ec1cca
Kevin Chen <chenk.dev@gmail.com>
2017-12-11 18:37:18 -0600

    why does the raycast shoot backwards sometimes...

--- GOTY-Roguelike-Game/Assets/Scripts/Player/Weapons/GunData.cs
+++ GOTY-Roguelike-Game/Assets/Scripts/Player/Weapons/GunData.cs
@@ -33,37 +33,50 @@ public class GunData : WeaponData {
 	private void RaycastShoot() {
 		StartCoroutine(ShotEffect());
 
-		RaycastHit hit;
+		RaycastHit camHit, shotHit;
 		this.line.SetPosition(0, shootPoint.position);
 		int layerMask = LayerMask.GetMask("Unwalkable", "Monster"); // Only collisions in layer Unwalkable and Monster
-        // raycast from 1 unit in front of camera and forward
-		if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward, Camera.main.transform.forward, out hit, range, layerMask)) {
-			this.line.SetPosition(1, hit.point);
-			RigCollider rigCollider = hit.transform.GetComponent<RigCollider>();
-			if (rigCollider == null) {
-				return;
-			}
-			Unit unit = rigCollider.RootUnit;
-			if (unit is AggressiveUnit) {
-				AggressiveUnit monster = (AggressiveUnit)unit;
-				float damage = this.damage;
-                damage *= damageMultiplier;
-                damage = WeaponEmotionActionHandler.GetOnDamageAction(emotion)(this, monster, damage);
-				damage = WeaponModifierActionHandler.GetOnDamageAction(modifier)(this, monster, damage);
-				monster.Damage(damage, Player.transform);
-				//PlayAttackAudio (1);
-			}
-			//if (hit.rigidbody != null) {
-			//	hit.rigidbody.AddForce(-hit.normal * hitForce);
-			//}
-		} else {
+        // shoot raycast out from camera
+		if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward, Camera.main.transform.forward, out camHit, range, layerMask)) {
+            // shoot raycast from shoot point to check for obstacles
+            Vector3 shootPointToCamHit = (camHit.point - shootPoint.position);
+            if (shootPointToCamHit.magnitude > 1f)
+                shootPointToCamHit.Normalize();
+            if (Physics.Raycast(shootPoint.position, shootPointToCamHit, out shotHit, range, layerMask))
+            {
+                this.line.SetPosition(1, shotHit.point);
+                RigCollider rigCollider = shotHit.transform.GetComponent<RigCollider>();
+                if (rigCollider == null)
+                {
+                    return;
+                }
+                Unit unit = rigCollider.RootUnit;
+                if (unit is AggressiveUnit)
+                {
+                    AggressiveUnit monster = (AggressiveUnit)unit;
+                    float damage = this.damage;
+                    damage *= damageMultiplier;
+                    damage = WeaponEmotionActionHandler.GetOnDamageAction(emotion)(this, monster, damage);
+                    damage = WeaponModifierActionHandler.GetOnDamageAction(modifier)(this, monster, damage);
+                    monster.Damage(damage, Player.transform);
+                    //PlayAttackAudio (1);
+                }
+                //if (hit.rigidbody != null) {
+                //	hit.rigidbody.AddForce(-hit.normal * hitForce);
+                //}
+            } else
+            { // player raycast hit nothing, though it should always hit something
+
+                this.line.SetPosition(1, shootPoint.position + (Camera.main.transform.forward * range));
+            }
+		} else { // camera raycast nothing
 			this.line.SetPosition(1, shootPoint.position + (Camera.main.transform.forward * range));
 		}
 	}
 
 	private IEnumerator ShotEffect() {
 		line.enabled = true;
-		yield return new WaitForSeconds(0.1f);
+		yield return new WaitForSeconds(0.05f);
 		line.enabled = false;
 	}
 
